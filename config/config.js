3a29fcd01fc847ada9256b999ef6a60e7313e509
sanjuconnectrader <sanjith@connectrader.io>
2025-06-24 02:01:35 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -7,21 +7,24 @@ export default {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    host:     process.env.DB_HOST,
+    port:     process.env.DB_PORT,      // <- added
+    dialect:  'mysql',
   },
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME_TEST,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    host:     process.env.DB_HOST,
+    port:     process.env.DB_PORT_TEST || process.env.DB_PORT, // optional override
+    dialect:  'mysql',
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
+    host:     process.env.DB_HOST,
+    port:     process.env.DB_PORT,      // <- added
+    dialect:  'mysql',
   },
 };
